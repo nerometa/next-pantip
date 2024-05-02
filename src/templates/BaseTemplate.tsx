6ad65f64dde1556c128341fa81547ceeff75dcec
nerometa <nerometa@protonmail.com>
2024-05-03 00:48:40 +0700

    feat(nav): sticky header

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -12,58 +12,60 @@ const BaseTemplate = (props: {
   const t = useTranslations('BaseTemplate');
 
   return (
-    <div className="w-full px-4 antialiased sm:px-8">
-      <div className="mx-auto">
-        <header className="border-b border-gray-300 py-4">
-          <div className="flex items-center justify-between">
-            <section id="main-nav" className="flex items-center">
-              <a href="/">
-                <Image
-                  width={80}
-                  height={0}
-                  src="/pantip-logo.png"
-                  alt="Pantip"
-                />
-              </a>
+    <>
+      <header className="fixed left-0 top-0 w-full border-b border-gray-300 bg-white p-4 sm:px-8">
+        <div className="flex items-center justify-between">
+          <section id="main-nav" className="flex items-center">
+            <a href="/">
+              <Image
+                width={80}
+                height={0}
+                src="/pantip-logo.png"
+                alt="Pantip"
+              />
+            </a>
 
-              <nav className="ml-8 hidden sm:block">
-                <ul className="flex flex-wrap gap-x-5">{props.leftNav}</ul>
-              </nav>
-            </section>
-
-            <nav className="hidden sm:block">
-              <ul className="flex flex-wrap items-center gap-x-5">
-                {props.rightNav}
-              </ul>
+            <nav className="ml-8 hidden sm:block">
+              <ul className="flex flex-wrap gap-x-5">{props.leftNav}</ul>
             </nav>
+          </section>
 
-            <nav id="mobile-nav" className="sm:hidden">
-              {props.mobileNav}
-            </nav>
-          </div>
-        </header>
+          <nav className="hidden sm:block">
+            <ul className="flex flex-wrap items-center gap-x-5">
+              {props.rightNav}
+            </ul>
+          </nav>
 
-        <main>{props.children}</main>
+          <nav id="mobile-nav" className="sm:hidden">
+            {props.mobileNav}
+          </nav>
+        </div>
+      </header>
 
-        <footer className="border-t border-gray-300 py-8 text-center text-sm">
-          © Copyright {new Date().getFullYear()} {AppConfig.name}.
-          {` ${t('made_with')} `}
-          <a
-            href="https://creativedesignsguru.com"
-            className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-          >
-            CreativeDesignsGuru
-          </a>
-          .
-          {/*
-           * PLEASE READ THIS SECTION
-           * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-           * The link doesn't need to appear on every pages, one link on one page is enough.
-           * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-           */}
-        </footer>
+      <div className="w-full px-4 antialiased sm:px-8">
+        <div className="mx-auto">
+          <main>{props.children}</main>
+
+          <footer className="border-t border-gray-300 py-8 text-center text-sm">
+            © Copyright {new Date().getFullYear()} {AppConfig.name}.
+            {` ${t('made_with')} `}
+            <a
+              href="https://creativedesignsguru.com"
+              className="text-blue-700 hover:border-b-2 hover:border-blue-700"
+            >
+              CreativeDesignsGuru
+            </a>
+            .
+            {/*
+             * PLEASE READ THIS SECTION
+             * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
+             * The link doesn't need to appear on every pages, one link on one page is enough.
+             * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
+             */}
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
